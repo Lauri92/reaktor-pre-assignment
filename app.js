709047302a596fdd4fe8e8949cd72d612e2de329
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-01-18 00:53:10 +0200

    port change

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.use(express.static('./'));
 
 app.use('/products', productRoute);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
